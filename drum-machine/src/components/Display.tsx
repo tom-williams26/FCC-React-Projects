80313d5ae262767f99d14779e457cab5cf42b673
Tom Williams <tom.williams0593@outlook.com>
2023-07-30 01:27:31 +0100

    refactoring display and button logic/components

--- drum-machine/src/components/Display.tsx
+++ drum-machine/src/components/Display.tsx
@@ -1,82 +1,94 @@
-import React, { useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import Button from './button/Button';
 import Controls from './controls/Controls';
 
 // Represents the object model.
-type ButtonAttributes = {
-	idIdentifier: string;
-	audioIdentifier: string;
+type AudioProps = {
+	key: string;
+	audio: string;
 	clip: string;
 };
 
-const audioClips: ButtonAttributes[] = [
+const audioClips: Array<AudioProps> = [
 	{
-		idIdentifier: 'Q',
-		audioIdentifier: 'Heater-1',
+		key: 'Q',
+		audio: 'Heater-1',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
 	},
 	{
-		idIdentifier: 'W',
-		audioIdentifier: 'Heater-2',
+		key: 'W',
+		audio: 'Heater-2',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
 	},
 	{
-		idIdentifier: 'E',
-		audioIdentifier: 'Heater-3',
+		key: 'E',
+		audio: 'Heater-3',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
 	},
 	{
-		idIdentifier: 'A',
-		audioIdentifier: 'Heater-4',
+		key: 'A',
+		audio: 'Heater-4',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
 	},
 	{
-		idIdentifier: 'S',
-		audioIdentifier: 'Clap',
+		key: 'S',
+		audio: 'Clap',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
 	},
 	{
-		idIdentifier: 'D',
-		audioIdentifier: 'Open-HH',
+		key: 'D',
+		audio: 'Open-HH',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
 	},
 	{
-		idIdentifier: 'Z',
-		audioIdentifier: "Kick-n'-Hat",
+		key: 'Z',
+		audio: "Kick-n'-Hat",
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
 	},
 	{
-		idIdentifier: 'X',
-		audioIdentifier: 'Kick',
+		key: 'X',
+		audio: 'Kick',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
 	},
 	{
-		idIdentifier: 'C',
-		audioIdentifier: 'Closed-HH',
+		key: 'C',
+		audio: 'Closed-HH',
 		clip: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
 	},
 ];
 
-const Display: React.FC = (audioClips) => {
+const Display = () => {
+	const [audio, setAudio] = useState<AudioProps[]>();
 	const [audioText, setAudioText] = useState<string>('');
 
-	// const buttonComponent = drumpadElement.map((element, key) => (
+	useEffect(() => {
+		setAudio(audioClips);
+	}, []);
+	console.log(audio);
+
+	// setAudio(audioClips)
+
+	// const buttonComponent = audioClips.map((element, key) => (
 	// 	<Button
 	// 		key={key}
-	// 		idIdentifier={element.idIdentifier}
-	// 		audioIdentifier={element.audioIdentifier}
+	// 		id={element.id}
+	// 		audio={element.audio}
 	// 		clip={element.clip}
 	// 	/>
 	// ));
 
-	const changeAudioText = (text: string) => {
-		setAudioText(text);
-	};
+	// const clips = () => {
+	// 	setAudio(audioClips);
+	// };
+	// const changeAudioText = (text: string) => {
+	// 	setAudioText(text);
+	// };
 
 	return (
 		<div id='display'>
 			<div className='display-container'>
-				<Button audioClips={audioClips} changeAudioText={changeAudioText} />
+				{/* {buttonComponent} */}
+				<Button props={audioClips} />
 			</div>
 			<div className='display-container' id='controls-container'>
 				<Controls />
--- drum-machine/src/components/button/Button.tsx
+++ drum-machine/src/components/button/Button.tsx
@@ -1,32 +1,43 @@
 import { FunctionComponent } from 'react';
 
-type AudioProps = {
-	audioClips: string;
-	changeAudioText: string;
+type ButtonProps = {
+	key: string;
+	audio: string;
+	clip: string;
 };
 
-// const Button: FunctionComponent<ButtonProps> = ({
-// 	idIdentifier,
-// 	audioIdentifier,
-// 	clip,
-// }) => {
+const Button = (props: ButtonProps) => {
+	return (
+		{props.map((element, key) => (
+				<Button
+					key={key}
+					key={element.key}
+					audio={element.audio}
+					clip={element.clip}
+				/>
+		))
+		}
+	)
+};
 
-// 	return (
-// 		<button className='drum-pad' id={audioIdentifier}>
-// 			<audio id={idIdentifier} src={clip} className='clip'></audio>
-// 			{idIdentifier}
+// const Button = ({ id, audio, clip }: AudioProps) => {
+// 		return (
+// 		<button className='drum-pad' id={id}>
+// 			<audio id={audio} src={clip} className='clip'></audio>
+// 			{id}
 // 		</button>
 // 	);
 // };
-const Button: FunctionComponent<AudioProps> = ({
-	audioClips,
-	changeAudioText,
-}) => {
-	return (
-		<button>
-			<audio></audio>
-		</button>
-	);
-};
+// return (
+// 		audioClips.map((element, key) => (
+// 				<Button
+// 					key={key}
+// 					idIdentifier={element.idIdentifier}
+// 					audioIdentifier={element.audioIdentifier}
+// 					clip={element.clip}
+// 				/>
+// 			))
+// );
+// };
 
 export default Button;
